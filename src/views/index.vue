3e199426750c6e1859ff5298fa43ac0d2c4a7c76
zrl <812588212@qq.com>
2019-11-06 08:58:53 +0800

    日考

---
+++ src/views/index.vue
@@ -0,0 +1,30 @@
+<!--  -->
+<template>
+    <div>
+        <div>
+            <span v-for="key in data" :key="key">
+                {{item.text}}
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+        data:[
+            {text:'学习 javascript'},
+            {text:'学习 vue'},
+            {text:'整个项目'}
+        ]
+    };
+  },
+
+ 
+}
+
+</script>
+<style lang='scss' scoped>
+
+</style>
